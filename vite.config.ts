ec811af699e97072ecd73f0b9e322180b603f239
Samuel3048 <samuelalegre022@gmail.com>
2023-08-20 14:01:46 -0400

    Config vite-test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest"/>
+/// <reference types="Vite/client"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +8,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    globals: true
   }
 })
