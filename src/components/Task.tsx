c4861640dc6075f01f9e7fde406bfda818896950
Samuel3048 <samuelalegre022@gmail.com>
2023-08-27 11:37:34 -0400

    adding confirmation message to delete a task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,6 +4,11 @@ import { type Todo as TodoType } from '../types'
 export const Task: React.FC<TodoType> = ({ id, completed, title }) => {
   const controller = useTasksDispatch()
 
+  const handleDelete = () => {
+    const deleteTask = window.confirm(`Do you really want to delete the task: "${title}`)
+    if (deleteTask) controller?.deleted(id)
+  }
+
   const stylesClass = completed
     ? {
         li: 'bg-lime-50',
@@ -17,10 +22,18 @@ export const Task: React.FC<TodoType> = ({ id, completed, title }) => {
       }
 
   return (
-    <li className={`border-b flex px-3 py-1 gap-3 justify-between ${stylesClass.li}`}>
-      <input type='checkbox' checked={completed} onChange={() => controller?.toggle(id)} />
-      <label className={`w-full text-lg overflow-ellipsis whitespace-nowrap ${stylesClass.p}`}>{title}</label>
-      <button className='col-span-1 place-self-center text-red-500' onClick={() => controller?.deleted(id)}>
+    <li className={`border-b flex  px-3 py-1 gap-3 justify-between ${stylesClass.li}`}>
+      <input
+        className='rounded-full peer relative appearance-none shrink-0 w-6 h-6 border-2 border-green-200  mt-1 bg-white
+        focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100
+        checked:bg-green-500 checked:border-0
+        disabled:border-steel-400 disabled:bg-steel-400'
+        type='checkbox'
+        checked={completed}
+        onChange={() => controller?.toggle(id)}
+      />
+      <label className={`w-full text-lg overflow-ellipsis ${stylesClass.p}`}>{title}</label>
+      <button className='col-span-1 place-self-center text-red-500' onClick={handleDelete}>
         x
       </button>
     </li>
