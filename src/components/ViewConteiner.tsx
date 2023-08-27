cb8039b55887d252aec2dc4f547f250f8989d3a0
Samuel3048 <samuelalegre022@gmail.com>
2023-08-27 11:39:25 -0400

    adding filter controller and task counter

--- src/components/ViewConteiner.tsx
+++ src/components/ViewConteiner.tsx
@@ -1,17 +1,26 @@
+import { useTasks } from '../contexts/task'
+import RenderController from './RenderController'
 import { TaskAdder } from './TaskAdder'
 import { TaskList } from './TaskList'
 import Today from './Today'
 
 export const ViewConteiner = () => {
+  const counter = useTasks()?.length
   return (
-    <main className='rounded-3xl shadow-lg bg-emerald-50 max-w-7xl m-auto overflow-hidden'>
-      <section className='px-5 pt-5 pb-2'>
-        <Today />
-      </section>
-      <TaskAdder />
-      <section className='main'>
-        <TaskList />
-      </section>
-    </main>
+    <>
+      <main className='rounded-3xl shadow-lg bg-emerald-50 max-w-7xl m-auto overflow-hidden'>
+        <section className='px-5 pt-5 pb-2'>
+          <Today />
+          <div className='flex justify-between'>
+            <span className='text-gray-500 text-lg'>{counter ?? 0} tasks</span>
+            <RenderController />
+          </div>
+        </section>
+        <TaskAdder />
+        <section className='main'>
+          <TaskList />
+        </section>
+      </main>
+    </>
   )
 }
